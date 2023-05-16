b515a7face02d7a05b96a6724dd7fe91e82af2bb
Odysseas <odykypar@gmail.com>
2023-05-16 21:57:32 +0200

    update

--- knowledge_graph_ABOX/src/main/java/ABoxCreator/ABoxCreator.java
+++ knowledge_graph_ABOX/src/main/java/ABoxCreator/ABoxCreator.java
@@ -2,14 +2,15 @@ package ABoxCreator;
 import org.apache.jena.ontology.OntDocumentManager;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.OWL;
+import org.apache.jena.vocabulary.RDF;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 public class ABoxCreator {
     public static OntModel loadTBoxFromResource(String resourcePath) {
@@ -33,14 +34,16 @@ public class ABoxCreator {
         return tboxModel;
     }
 
-    public static void loadABoxData(OntModel aboxModel, String resourcePath, String baseURL) {
+    public static void loadABoxData(OntModel aboxModel, String resourcePath, String baseURL, OntModel tboxModel) {
         // Load the input stream for the resource file
         InputStream in = ABoxCreator.class.getResourceAsStream(resourcePath);
 
         // Process the CSV data and create individuals in the ABox
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
             String line;
+
             boolean isFirstLine = true;
+            String previousPaperID = "xxxxx";
 
             // Read each line of the CSV file
             while ((line = reader.readLine()) != null) {
@@ -54,32 +57,62 @@ public class ABoxCreator {
                 String[] columns = splitCSVLine(line);
 
                 // Extract relevant data from the columns
-                String paperID = columns[0].trim();
-                String paperTitle = columns[1].trim();
-                String authorID = columns[2].trim();
-                String authorName = columns[3].trim();
-                String paperType = columns[4].trim();
-                String conferenceType = columns[5].trim();
-                String sourceID = columns[6].trim();
-                String source = columns[7].trim();
+                String paperID = prepare_str(columns[0].trim());
+                String paperTitle = prepare_str(columns[1].trim());
+                String authorID = prepare_str(columns[2].trim());
+                String authorName = prepare_str(columns[3].trim());
+                String paperType = prepare_str(columns[4].trim());
+                String conferenceType = prepare_str(columns[5].trim());
+                String sourceID = prepare_str(columns[6].trim());
+                String source = prepare_str(columns[7].trim());
                 int year = Integer.parseInt(columns[8].trim());
-                String documentType = columns[9].trim();
-                String volumeProceeding = columns[10].trim();
-                String reviewer1ID = columns[11].trim();
-                String reviewer1 = columns[12].trim();
-                String reviewer2ID = columns[13].trim();
-                String reviewer2 = columns[14].trim();
-                String handlerID = columns[15].trim();
-                String handler = columns[16].trim();
-                String handlerType = columns[17].trim();
-                String areaID = columns[18].trim();
-                String area = columns[19].trim();
-                String reviewID = columns[20].trim();
-                String reviewerDecision = columns[21].trim();
+                String documentType = prepare_str(columns[9].trim());
+                String volumeProceeding = prepare_str(columns[10].trim());
+                String reviewer1ID = prepare_str(columns[11].trim());
+                String reviewer1 = prepare_str(columns[12].trim());
+                String reviewer2ID = prepare_str(columns[13].trim());
+                String reviewer2 = prepare_str(columns[14].trim());
+                String handlerID = prepare_str(columns[15].trim());
+                String handler = prepare_str(columns[16].trim());
+                String handlerType = prepare_str(columns[17].trim());
+                String areaID = prepare_str(columns[18].trim());
+                String area = prepare_str(columns[19].trim());
+                String reviewID = prepare_str(columns[20].trim());
+                String reviewerDecision = prepare_str(columns[21].trim());
                 String reviewerText = columns[22].trim();
-                String proceedingVolume = columns[23].trim();
-                String proceedingVolumeID = columns[24].trim();
+                String proceedingVolume = prepare_str(columns[23].trim());
+                String proceedingVolumeID = prepare_str(columns[24].trim());
 
+                Boolean isConference = documentType.equals("Conference");
+                Boolean isAccepted = reviewerDecision.equals("Accepted");
+
+                Resource paper;
+                if (paperType.equals("Full_Paper")) {
+                    paper = tboxModel.getResource(baseURL.concat(TBoxVariables.FULL_PAPER));
+                } else if (paperType.equals("Short_Paper")) {
+                    paper = tboxModel.getResource(baseURL.concat(TBoxVariables.SHORT_PAPER));
+                } else if (paperType.equals("Demo_Paper")) {
+                    paper = tboxModel.getResource(baseURL.concat(TBoxVariables.DEMO_PAPER));
+                } else {
+                    paper = tboxModel.getResource(baseURL.concat(TBoxVariables.POSTER));
+                }
+
+                // This block refers to all the rows of the same paper.
+                if (paperID.equals(previousPaperID)) {
+
+
+
+
+
+
+                    previousPaperID = paperID; // change previousPaperID to show the one that inserted now
+
+                } else {
+
+                    previousPaperID = paperID;
+
+
+                }
                 // proceedingVolume = "No"  -> Review Decision = "Rejected"
                 // proceedingVolume != "No" -> proceedingVolume contains Conference Proceeding or Journal Volume
                 // and proceedingVolumeID the ID of Conference Proceeding or Journal Volume respectively
@@ -137,10 +170,10 @@ public class ABoxCreator {
         }
     }
 
-    public static void saveABoxOntology(OntModel aboxModel, String outputPath) {
+    public static void saveABoxOntology(OntModel aboxModel, String outputPath, String baseURL) {
         try (OutputStream out = new FileOutputStream(outputPath)) {
-            // Set the RDF/XML format for serialization
-            aboxModel.setNsPrefix("ex", "http://example.com/ontology/");
+            // Set the base URL for serialization
+            aboxModel.setNsPrefix("", baseURL);
             aboxModel.write(out, "RDF/XML");
             System.out.println("ABox ontology saved to: " + outputPath);
         } catch (IOException e) {
@@ -199,5 +232,166 @@ public class ABoxCreator {
     public static boolean resourceExist(OntModel aboxModel, String baseURL, String className, String id) {
         String resourceURI = baseURL + "/" + className + "#" + id;
         return aboxModel.containsResource(aboxModel.getResource(resourceURI));
+    }
+
+    public static String prepare_str(String str) {
+        return str
+                .replace("; ", ";")
+                .replace(".", "_")
+                .replace(",", "_")
+                .replace(": ", ":")
+                .replace("'s", "_s")
+                .replace(" ", "_")
+                .replace("-", "_")
+                .replace("’", "")
+                .replace("©", "")
+                .replace("›", "")
+                .replace("‹", "")
+                .replace("°", "")
+                .replace("%", "")
+                .replace("$", "")
+                .replace("@", "")
+                .replace("!", "")
+                .replace("&", "")
+                .replace("~", "")
+                .replace("'", "")
+                .replace("+", "")
+                .replace("ν", "")
+                .replace("η", "")
+                .replace("?", "")
+                .replace(">", "")
+                .replace("<", "")
+                .replace("μ", "")
+                .replace("\"", "")
+                .replace("\\", "")
+                .replace("^", "")
+                .replace("#", "")
+                .replaceAll("[\\p{Ps}\\p{Pe}]", "") // To remove all opening & closing brackets (https://stackoverflow.com/a/25853119/6390175)
+                .replaceAll("[^A-Za-z0-9] ","")
+                ;
+    }
+
+    public static void printTBoxElements(Model tboxModel) {
+        Set<Property> objectPropertySet = getObjectProperties(tboxModel);
+        Set<Property> dataPropertySet = getDataProperties(tboxModel);
+        Set<Resource> classSet = getClasses(tboxModel);
+
+        // Print the retrieved object properties
+        System.out.println("Object Properties:");
+        for (Property objectProperty : objectPropertySet) {
+            System.out.println(objectProperty.getURI());
+        }
+
+        // Print the retrieved data properties
+        System.out.println("Data Properties:");
+        for (Property dataProperty : dataPropertySet) {
+            System.out.println(dataProperty.getURI());
+        }
+
+        // Print the retrieved classes
+        System.out.println("Classes:");
+        for (Resource classResource : classSet) {
+            System.out.println(classResource.getURI());
+        }
+    }
+
+    private static Set<Property> getObjectProperties(Model tboxModel) {
+        StmtIterator objectProperties = tboxModel.listStatements(null, RDF.type, OWL.ObjectProperty);
+        Set<Property> objectPropertySet = new HashSet<>();
+        while (objectProperties.hasNext()) {
+            Statement statement = objectProperties.next();
+            objectPropertySet.add(statement.getSubject().as(Property.class));
+        }
+        return objectPropertySet;
+    }
+
+    private static Set<Property> getDataProperties(Model tboxModel) {
+        StmtIterator dataProperties = tboxModel.listStatements(null, RDF.type, OWL.DatatypeProperty);
+        Set<Property> dataPropertySet = new HashSet<>();
+        while (dataProperties.hasNext()) {
+            Statement statement = dataProperties.next();
+            dataPropertySet.add(statement.getSubject().as(Property.class));
+        }
+        return dataPropertySet;
+    }
+
+    private static Set<Resource> getClasses(Model tboxModel) {
+        StmtIterator classes = tboxModel.listStatements(null, RDF.type, OWL.Class);
+        Set<Resource> classSet = new HashSet<>();
+        while (classes.hasNext()) {
+            Statement statement = classes.next();
+            classSet.add(statement.getSubject());
+        }
+        return classSet;
+    }
+
+    public class TBoxVariables {
+        // Object Properties
+        public static final String HANDLED_BY_EDITOR = "handledByEditor";
+        public static final String HANDLES_CONFERENCE = "handlesConference";
+        public static final String INCLUDED_IN_JOURNAL_VOLUME = "includedInJournalVolume";
+        public static final String INCLUDED_IN = "includedIn";
+        public static final String HANDLED_BY_CHAIR = "handledByChair";
+        public static final String HANDLES_JOURNAL = "handlesJournal";
+        public static final String ASSIGNS_REVIEWER = "assignsReviewer";
+        public static final String HAS_SUBMITTED = "hasSubmitted";
+        public static final String HAS_POSITIVE_REVIEW_SUBMITTED = "hasPositiveReviewSubmitted";
+        public static final String HAS_AUTHOR = "hasAuthor";
+        public static final String HAS_NEGATIVE_REVIEW_SUBMITTED = "hasNegativeReviewSubmitted";
+        public static final String CHAIR_ASSIGNS_REVIEWER = "chairAssignsReviewer";
+        public static final String SUBMITS_POSITIVE_REVIEW = "submitsPositiveReview";
+        public static final String VENUE_IS_RELATED_TO = "venueIsRelatedTo";
+        public static final String IS_SUBMITTED = "isSubmitted";
+        public static final String HAS_REVIEWER = "hasReviewer";
+        public static final String HAS_REVIEW_SUBMITTED = "hasReviewSubmitted";
+        public static final String HANDLES = "handles";
+        public static final String BEING_HANDLED_BY = "beingHandledBy";
+        public static final String EDITOR_ASSIGNS_REVIEWER = "editorAssignsReviewer";
+        public static final String SUBMITS_NEGATIVE_REVIEW = "submitsNegativeReview";
+        public static final String SUBMITS_REVIEW = "submitsReview";
+        public static final String INCLUDED_IN_CONFERENCE_PROCEEDINGS = "includedInConferenceProceedings";
+        public static final String IS_RELATED_TO = "isRelatedTo";
+        public static final String PAPER_IS_RELATED_TO = "paperIsRelatedTo";
+
+        // Data Properties
+        public static final String HAS_POSITIVE_DECISION = "hasPositiveDecision";
+        public static final String JOURNAL_VOLUME_HAS_YEAR = "journalVolumeHasYear";
+        public static final String CONFERENCE_PROCEEDINGS_HAS_YEAR = "conferenceProceedingsHasYear";
+        public static final String AREA_HAS_NAME = "areaHasName";
+        public static final String PERSON_HAS_NAME = "personHasName";
+        public static final String HAS_YEAR = "hasYear";
+        public static final String VENUE_HAS_NAME = "venueHasName";
+        public static final String HAS_NEGATIVE_DECISION = "hasNegativeDecision";
+        public static final String HAS_DECISION = "hasDecision";
+        public static final String HAS_NAME = "hasName";
+        public static final String HAS_TEXT = "hasText";
+        public static final String HAS_TITLE = "hasTitle";
+
+        // Classes
+        public static final String DEMO_PAPER = "DemoPaper";
+        public static final String CONFERENCE_PROCEEDINGS = "ConferenceProceedings";
+        public static final String PAPER = "Paper";
+        public static final String CHAIR = "Chair";
+        public static final String WORKSHOP = "Workshop";
+        public static final String EDITOR = "Editor";
+        public static final String EXPERT_GROUP = "ExpertGroup";
+        public static final String POSTER = "Poster";
+        public static final String JOURNAL_VOLUME = "JournalVolume";
+        public static final String SUBMITTED_PAPER = "SubmittedPaper";
+        public static final String VENUE = "Venue";
+        public static final String REGULAR_CONFERENCE = "RegularConference";
+        public static final String CONFERENCE = "Conference";
+        public static final String NEGATIVE_REVIEW = "NegativeReview";
+        public static final String REVIEW = "Review";
+        public static final String AREA = "Area";
+        public static final String SHORT_PAPER = "ShortPaper";
+        public static final String AUTHOR = "Author";
+        public static final String FULL_PAPER = "FullPaper";
+        public static final String POSITIVE_REVIEW = "PositiveReview";
+        public static final String ACCEPTED_PAPER = "AcceptedPaper";
+        public static final String JOURNAL = "Journal";
+        public static final String SYMPOSIUM = "Symposium";
+        public static final String PERSON = "Person";
+        public static final String REVIEWER = "Reviewer";
     }
 }
--- knowledge_graph_ABOX/src/main/java/Main.java
+++ knowledge_graph_ABOX/src/main/java/Main.java
@@ -14,17 +14,17 @@ public class Main {
         String baseURL = ABoxCreator.getBaseURL(tboxModel);
         System.out.println("Base URL: " + baseURL);
 
+        // Check the Elements included in the TBOX model
+        ABoxCreator.printTBoxElements(tboxModel);
+
         // Create the ABox ontology
          OntModel aboxModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF, tboxModel);
 
         // Load data from instances.csv and create individuals in the ABox
-        ABoxCreator.loadABoxData(aboxModel, "/abox_data.csv", baseURL);
+        ABoxCreator.loadABoxData(aboxModel, "/abox_data.csv", baseURL, tboxModel);
 
         // Save the ABox ontology to a file
-//        String namespaceURI = tboxModel.getNsPrefixURI("ex");  // Retrieve the namespace URI from the TBox model
-
-        // Save the ABox ontology to a file
-//        ABoxCreator.saveABoxOntology(aboxModel, "abox.rdf");
+        ABoxCreator.saveABoxOntology(aboxModel, "abox.rdf", baseURL);
 
         System.out.println("We have aboxModel!");
     }
